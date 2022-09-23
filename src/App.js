befe1a331e7629cc1ab322a005e6c9708368d905
Jordane YENO ADEMBIGONDJO <adembigondjo@gmail.com>
2022-09-23 18:33:54 +0100

    'app'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p> Compteur: <h3>{count}</h3></p>
+
+      <div className='Selector'>
+        <button onClick={() => setCount(count - 1)}>Decrementation</button>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+      </div>
+
     </div>
   );
 }
